9bea86c5b44f6b42c7f36ab1bfbb0107db91be00
JasinskiyD <dmitriy.jasinskiy@gmail.com>
2017-12-10 11:50:04 +0200

    master commit

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
 
     public static void main(String[] args) {
         String hello = "Hello ";
+        String peace = "peaceful ";
         String world = "World!";
         System.out.println(hello + world);
     }
