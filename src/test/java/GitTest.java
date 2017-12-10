d8fe129ad11a0fda8cd6ae1c0d8a67969df9df9b
JasinskiyD <dmitriy.jasinskiy@gmail.com>
2017-12-10 11:46:09 +0200

    branch2

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -1,8 +1,6 @@
 public class GitTest {
 
     public static void main(String[] args) {
-        String hello = "Hello ";
-        String world = "World!";
-        System.out.println(hello + world);
+        System.out.println("hello world!");
     }
 }
